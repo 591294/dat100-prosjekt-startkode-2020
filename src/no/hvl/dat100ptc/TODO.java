c30b68f1be51110442630079810d115d0bcb3a7a
591294 <oyrsl@Lenovo-PC>
2020-10-13 20:57:58 +0200

    Ø UTF-8

--- src/no/hvl/dat100ptc/TODO.java
+++ src/no/hvl/dat100ptc/TODO.java
@@ -12,7 +12,7 @@ public class TODO {
 	
 	public static String construtor(String className) {
 				
-	   return "Konstruktøren for klassen " + className + " er ikke implementert";
+	   return "KonstruktÃ¸ren for klassen " + className + " er ikke implementert";
 		
 	}
 
